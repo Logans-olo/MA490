2c9ac5f939f11601656bfefad48a901435ff6372
Logan-solo <lsolonch@purdue.edu>
2026-02-02 19:38:44 -0500

    Added multi stage diagonalizing

--- decomposition.py
+++ decomposition.py
@@ -18,25 +18,24 @@ def init():
     matrix += np.eye(n) * 1
     return matrix
 
-def decomp(matrix):
+def decomp(matrix, i0, i1, i2):
     """This function takes a 2 x 2 matrix and decomposes it such that the first off-diagonal is removed
     This is done via a replacement with the Schur componenet
 
     Args:
         matrix (int): A 2x2 matrix that should be dense.
     """
-    k = 2
+    
 
-    A  = matrix[0:k, 0:k]
-    D  = matrix[0:k, k:2*k]
-    Dt = matrix[k:2*k, 0:k]
-    B  = matrix[k:2*k, k:2*k]
+    A  = matrix[i0:i1, i0:i1]
+    D  = matrix[i0:i1, i1:i2]
+    Dt = matrix[i1:i2, i0:i1]
+    B  = matrix[i1:i2,  i1:i2]
     X = np.linalg.solve(A, D)
     B = B - Dt @ X
-    matrix[0:k, 0:k] = A
-    matrix[0:k, k:2*k] = 0
-    matrix[k:2*k, 0:k] = 0
-    matrix[k:2*k, k:2*k] = B
+    matrix[i1:i2, i1:i2] = B
+    matrix[i0:i1, i1:i2] = 0
+    matrix[i1:i2, i0:i1] = 0
     # print(matrix)
 
 if __name__ == "__main__":
@@ -44,5 +43,13 @@ if __name__ == "__main__":
         print("Provide a Matrix Size, and a bandwidth constants")
         exit(1)
     matrix = init()
+    n = matrix.shape[0]
     # print(matrix)
-    decomp(matrix)
+    k = 2
+    for i in range(0, n-k, k): 
+        i0 = i
+        i1 = i + k
+        i2 = i + 2 * k
+        decomp(matrix, i0,i1,i2)
+        
+    print(matrix)
