7ee5109fc511b560d1a025c8f2ae2bfc9d095efe
Logan-solo <lsolonch@purdue.edu>
2026-03-09 18:35:06 -0400

    Skeleton random adj. matrix + completion number solving

--- runners/products.py
+++ runners/products.py
@@ -1,9 +1,27 @@
 from utils import clique
 from utils import products
 import numpy as np
+import cvxpy as cp
+
+n = 4
+p = 0.3
+
 def run():
-    A = np.zeros((4,4))
-    B = np.zeros((4,4))
     
+    A = random_graph(n,p)
+    print(np.linalg.eigvals(A))
+    B = random_graph(n,p)
+    print(np.linalg.eigvals(B))
+    P = products.cartesian_product(A, B)
+    print(np.linalg.eigvals(P))
+    P = products.tensor_product(A, B)
+    print(np.linalg.eigvals(P))
+def random_graph(n, p):
+    M = np.random.rand(n, n) < p
+    A = np.triu(M, 1)
+    A = A + A.T  # make symmetric (undirected)
+    np.fill_diagonal(A, 0)
+    return A
+
 if __name__ == "__main__":
     run()
--- utils/products.py
+++ utils/products.py
@@ -10,10 +10,10 @@ def cartesian_product(A: np.ndarray, B: np.ndarray):
     G = nx.from_numpy_array(A)
     H = nx.from_numpy_array(B)
     P = nx.cartesian_product(G, H)
-    return np.array(nx.adjacency_matrix(P))
+    return nx.to_numpy_array(P, dtype=int)
 
 def tensor_product(A: np.ndarray, B: np.ndarray):
     G = nx.from_numpy_array(A)
     H = nx.from_numpy_array(B)
     P = nx.tensor_product(G, H)
-    return np.array(nx.adjacency_matrix(P))
+    return nx.to_numpy_array(P, weight=None, dtype=int)
