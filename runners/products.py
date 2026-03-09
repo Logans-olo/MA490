b57ca849dd7425175390dc85ecb1604860d0754c
Logan-solo <lsolonch@purdue.edu>
2026-03-09 15:44:57 -0400

    Very simple graph products using NX

---
+++ runners/products.py
@@ -0,0 +1,9 @@
+from utils import clique
+from utils import products
+import numpy as np
+def run():
+    A = np.zeros((4,4))
+    B = np.zeros((4,4))
+    
+if __name__ == "__main__":
+    run()
---
+++ utils/products.py
@@ -0,0 +1,19 @@
+import networkx as nx 
+import numpy as np
+"""
+Very simple set of functions for testing graph products.
+These are simply warreprs over Networkx functions
+Designed to be used with Numpy Arrays
+
+"""
+def cartesian_product(A: np.ndarray, B: np.ndarray):
+    G = nx.from_numpy_array(A)
+    H = nx.from_numpy_array(B)
+    P = nx.cartesian_product(G, H)
+    return np.array(nx.adjacency_matrix(P))
+
+def tensor_product(A: np.ndarray, B: np.ndarray):
+    G = nx.from_numpy_array(A)
+    H = nx.from_numpy_array(B)
+    P = nx.tensor_product(G, H)
+    return np.array(nx.adjacency_matrix(P))
